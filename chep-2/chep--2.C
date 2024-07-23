9c0d07715acad1e8f6d14b3d3cc4ace54a1cd8bb
Parth-Kachariya <parthkachariya668@gmail.com>
2024-07-23 12:26:24 +0530

    Add files via upload

---
+++ chep-2/chep--2.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+#define p printf
+#define s scanf
+
+#define m main
+m()
+{
+       int l;
+       int b;
+       float pi=3.14;
+       int r;
+
+       
+       p("enter the value of area:");
+       s("%d",&l);
+       p("area of squre:%d\n",l*l);
+       p("enter the value of area:");
+       s("%d",&b);
+       p("area of rectangale:%d\n",l*b);
+       p("enter the value of area:");
+       s("%d",&pi);
+       p("area of cricle:%f\n",pi*r*r);
+
+      
+}
---
+++ chep-2/chep-2.C
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+
+main ()
+{          int l;
+	   int b;
+	   float pi;
+	   int r;
+	   int h;
+	   int p;
+	   int n=40;
+
+	   
+	   printf("area of square: sss%d\n",l*l);
+	   printf("area of rectangal%d\n",l*b);
+	   printf("area of cricle=%.2f\n",pi*7*7);
+	   printf("permetars of circle=%.2f\n",2*pi*r);
+	   printf("area of triangle=%.2f\n",0.5*h*l);
+	   printf("simpale intrsat=%d\n",p*r*n/100);
+
+	   
+
+}
