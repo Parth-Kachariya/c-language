2d5eea6ea33fdacd28b0bceb4e8dda8a09b305a2
Parth-Kachariya <parthkachariya668@gmail.com>
2024-07-23 12:24:07 +0530

    Add files via upload

---
+++ chep-1/chep-1.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdio.h>
+
+
+main()
+{
+	  clrscr();
+	  printf("hello c language\n");
+	  printf("name\t\t:kachariya parth h.\n");
+	  printf("age\t\t:18\n");
+	  printf("add\t\t:nandaniketan so. variyav\n");
+	  printf("gendar\t\t:male\n");
+	  printf("married\t\t:un married\n");
+	  printf("mobile no\t:9034286085\n");
+	  printf("dob\t\t:14/6/2006\n");
+	  getch();
+
+
+
+
+}
