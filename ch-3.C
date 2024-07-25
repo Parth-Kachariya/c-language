b68ae0c0b3616c6d3f906f87c9dd9758ca615ed4
Parth-Kachariya <parthkachariya668@gmail.com>
2024-07-25 12:12:27 +0530

    Add files via upload

---
+++ ch-3.C
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+main()
+{     float c,f;
+      
+      printf("enter value:");
+      scanf("%f",&c);
+      f=(c*9/5)+32;
+      printf("fahr:%.2f",f);
+      
+}
---
+++ chep-3.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+main()
+{
+
+	      int a,b;
+
+	      
+	      printf("enter the value of a:");
+	      scanf("%d",&a);
+	     
+	      printf("enter the value of b:");
+	      scanf("%d",&b);
+	      printf("value of b:%d",b);
+	      printf("sum of %d and %d:%d\n",a,b,a+b);
+	      printf("multiplication:%d\n",a*b);
+	      printf("divison:%d\n",a/b);
+	      printf("mines:%d\n",a-b);
+	      printf("persanttage:%d\n",a%b);
+
+	      
+
+
+}
